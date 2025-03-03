86ea6e1eea37e175bd61eba8edfdf397d7dfb2b4
Akrez <akrez.like@gmail.com>
2025-03-03 22:58:41 +0330

    WebResponse

---
+++ app/Support/WebResponse.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Support;
+
+class WebResponse extends ApiResponse
+{
+    private ?string $successfulRoute;
+
+    public function reset(): self
+    {
+        parent::reset();
+        $this->successfulRoute = null;
+
+        return $this;
+    }
+
+    public function successfulRoute(?string $successfulRoute): self
+    {
+        $this->successfulRoute = $successfulRoute;
+
+        return $this;
+    }
+
+    public function getSuccessfulRoute(): ?string
+    {
+        return $this->successfulRoute;
+    }
+
+    public function toResponse($request)
+    {
+        if (! $this->isSuccessful()) {
+            return back()
+                ->with('swal-error', $this->getMessage())
+                ->withInput($request->input())
+                ->withErrors($this->getErrors());
+        }
+
+        if ($this->getSuccessfulRoute() === null) {
+            return back()
+                ->with('swal-success', $this->getMessage());
+        }
+
+        return redirect()
+            ->to($this->getSuccessfulRoute())
+            ->with('swal-success', $this->getMessage());
+    }
+}
