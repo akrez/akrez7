68ee73fba2aef97ae33ed7144f9d2c5e96a50311
Akrez <akrez.like@gmail.com>
2025-03-22 16:52:16 +0330

    up

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -22,11 +22,11 @@
                 <div class="card h-100">
                     <img src="{{ \Arr::get($gallery, 'url') }}" class="card-img-top">
                     <div class="card-body d-flex flex-column justify-content-end">
-                        <p class="card-text mb-0">
+                        <div class="card-text">
                             <div> <strong>@lang('validation.attributes.is_selected'): </strong>{{ $gallery['selected_at'] ? __('Yes') : __('No') }}</div>
                             <div> <strong>@lang('validation.attributes.gallery_order'): </strong>{{ $gallery['gallery_order'] }}</div>
-                        </p>
-                        <a href="{{ route('galleries.edit', ['id' => $gallery['id']]) }}" class="btn btn-default border border-dark w-100">
+                        </div>
+                        <a href="{{ route('galleries.edit', ['id' => $gallery['id']]) }}" class="btn btn-default border border-dark w-100 mt-4">
                             @lang('Edit')
                         </a>
                         <x-form :action="route('galleries.destroy', ['id' => $gallery['id']])" method="DELETE">
