66f0092807fdf02cfd7f1595541039284acb0018
Akrez <akrez.like@gmail.com>
2025-04-04 23:16:26 +0330

    up

--- database/migrations/2025_03_03_195936_add_active_blog_to_users_table.php
+++ database/migrations/2025_03_03_195936_add_active_blog_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('active_blog')->index();
+            $table->unsignedBigInteger('active_blog')->nullable()->index();
         });
     }
 
