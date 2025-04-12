64dea925eca1b6707ca0f70ce0eb4eb427f2a4d7
Akrez <akrez.like@gmail.com>
2025-04-12 23:21:19 +0330

    TelegramMessage

--- app/Models/TelegramMessage.php
+++ app/Models/TelegramMessage.php
@@ -7,41 +7,46 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class TelegramMessage
- *
+ * 
  * @property int $id
- * @property int|null $chat_id
+ * @property string $telegram_token
  * @property array|null $message_json
- * @property string|null $processor
- * @property int $blog_id
- * @property int $bot_id
- * @property Carbon|null $responsed_at
+ * @property string|null $process_status
+ * @property int|null $blog_id
+ * @property int|null $bot_id
+ * @property int|null $chat_id
  * @property string|null $deleted_at
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ *
+ * @package App\Models
  */
 class TelegramMessage extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
 
-    protected $table = 'telegram_messages';
+	protected $table = 'telegram_messages';
 
-    public $incrementing = false;
+	public $incrementing = false;
 
-    protected $casts = [
-        'id' => 'int',
-        'chat_id' => 'int',
-        'message_json' => 'json',
-        'blog_id' => 'int',
-        'bot_id' => 'int',
-        'responsed_at' => 'datetime',
-    ];
+	protected $casts = [
+		'id' => 'int',
+		'message_json' => 'json',
+		'blog_id' => 'int',
+		'bot_id' => 'int',
+		'chat_id' => 'int'
+	];
 
-    protected $fillable = [
-        'chat_id',
-        'message_json',
-        'processor',
-        'blog_id',
-        'bot_id',
-        'responsed_at',
-    ];
+	protected $hidden = [
+		'telegram_token'
+	];
+
+	protected $fillable = [
+		'telegram_token',
+		'message_json',
+		'process_status',
+		'blog_id',
+		'bot_id',
+		'chat_id'
+	];
 }
