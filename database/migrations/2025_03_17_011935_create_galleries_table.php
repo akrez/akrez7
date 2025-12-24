4e36542416bb6751955aef2ccdb627fa5d0d7f52
akrez <akrez.like@gmail.com>
2025-12-25 03:18:08 +0330

    up

--- database/migrations/2025_03_17_011935_create_galleries_table.php
+++ database/migrations/2025_03_17_011935_create_galleries_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
             $table->string('name', 60)->index();
-            $table->morphs('gallery');
+            $table->unsignedBigInteger('gallery_id');
             $table->unsignedBigInteger('blog_id')->index();
             $table->string('ext', 8);
             $table->string('gallery_category');
