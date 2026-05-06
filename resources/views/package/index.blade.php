0f47f2111da69ad84f53651fb51823202f6f49c0
akrez <akrez.like@gmail.com>
2026-05-06 22:05:29 +0330

    up

--- resources/views/package/index.blade.php
+++ resources/views/package/index.blade.php
@@ -9,6 +9,11 @@
 @endphp
 
 @section('content')
+    <style>
+        .form-select {
+            --bs-form-select-bg-img: none;
+        }
+    </style>
 
     <div class="row" x-data="data()" x-init="initData({{ json_encode($params) }})">
         <div class="table-responsive">
@@ -36,31 +41,66 @@
                                     x-show="packageIndex === 0"></td>
                                 <td class="" x-bind:rowspan="Object.keys(packages[product.id]).length" x-text="'+'"
                                     x-show="packageIndex === 0"></td>
-                                <td class="">
+                                <td
+                                    :class="{
+                                        'bg-info': strval(packages[product.id][packageIndex]['price']) !==
+                                            strval(packages_const[package.id]['price'])
+                                    }">
                                     <input class="form-control p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['price']">
                                 </td>
-                                <td class="">
+
+                                <td
+                                    :class="{
+                                        'bg-info': boolval(packages[product.id][packageIndex]['show_price']) !=
+                                            boolval(packages_const[package.id]['show_price'])
+                                    }">
                                     <select class="form-select p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['show_price']">
-                                        <option :selected="!packages[product.id][packageIndex]['show_price']"
-                                            value="">@lang('No')</option>
-                                        <option :selected="packages[product.id][packageIndex]['show_price']" value="1">
-                                            @lang('Yes')</option>
+                                        <template x-for="(show_price, show_price_key) in show_prices">
+                                            <option
+                                                :selected="boolval(show_price_key) == boolval(getPackage(product?.id, packageIndex)
+                                                    ?.show_price)"
+                                                :value="boolval(show_price_key)" x-text="show_price">
+                                            </option>
+                                        </template>
                                     </select>
                                 </td>
-                                <td class="">
-                                    <input class="form-control p-1 text-center"
-                                        x-model="packages[product.id][packageIndex]['status']">
+                                <td
+                                    :class="{
+                                        'bg-info': packages[product.id][packageIndex]['package_status']['value'] !==
+                                            packages_const[package.id]['package_status']['value']
+                                    }">
+                                    <select class="form-select p-1 text-center"
+                                        x-model="packages[product.id][packageIndex]['package_status']['value']">
+                                        <template x-for="(package_status, package_status_key) in package_statuses"
+                                            :key="package_status_key">
+                                            <option
+                                                :selected="package_status_key == getPackage(product?.id, packageIndex)
+                                                    ?.package_status['value']"
+                                                :value="package_status_key" x-text="package_status">
+                                            </option>
+                                        </template>
+                                    </select>
                                 </td>
-                                <td class="">
+
+
+                                <td
+                                    :class="{
+                                        'bg-info': (packages[product.id][packageIndex]['unit']) !==
+                                            (packages_const[package.id]['unit'])
+                                    }">
                                     <input class="form-control p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['unit']">
                                 </td>
-                                <td class="">
+                                <td
+                                    :class="{
+                                        'bg-info': parseInt(packages[product.id][packageIndex]['color_id']) !==
+                                            parseInt(packages_const[package.id]['color_id'])
+                                    }">
                                     <select class="form-select p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['color_id']">
-                                        <option :value="''"></option>
+                                        <option></option>
                                         <template x-for="color in colors" :key="color.id">
                                             <option :selected="color.id == getPackage(product?.id, packageIndex)?.color_id"
                                                 :value="color.id" x-text="color.name"
@@ -72,15 +112,25 @@
                                         </template>
                                     </select>
                                 </td>
-                                <td class="">
+                                <td
+                                    :class="{
+                                        'bg-info': strval(packages[product.id][packageIndex]['guaranty']) !=
+                                            strval(packages_const[package.id]['guaranty'])
+                                    }">
                                     <input class="form-control p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['guaranty']">
                                 </td>
-                                <td class="">
+                                <td
+                                    :class="{
+                                        'bg-info': strval(packages[product.id][packageIndex]['description']) !=
+                                            strval(packages_const[package.id]['description'])
+                                    }">
                                     <input class="form-control p-1 text-center"
                                         x-model="packages[product.id][packageIndex]['description']">
                                 </td>
-                                <td class=""></td>
+                                <td class="">
+                                    <div class="btn btn-primary p-1">@lang('Update')</div>
+                                </td>
                                 <td class=""></td>
                             </tr>
                         </template>
@@ -103,9 +153,11 @@
                 base_url: null,
                 products: [],
                 packages: [],
+                packages_const: [],
                 colors: [],
                 list: [],
-                opened: [],
+                package_statuses: [],
+                show_prices: [],
                 loading: {
                     syncLists: false
                 },
@@ -141,6 +193,16 @@
                 getPackage(productId, packageIndex) {
                     return this.packages[productId][packageIndex];
                 },
+                boolval(value) {
+                    if (value === 'false') return 0;
+                    if (value === 'null') return 0;
+                    if (value === '0') return 0;
+                    return Boolean(value) ? 1 : 0;
+                },
+                strval(value) {
+                    if (value === null) return '';
+                    return String(value);
+                },
                 async syncLists() {
                     try {
                         if (this.loading.syncLists) {
@@ -156,22 +218,26 @@
                         let json = await res.json();
 
                         products = json.data.products || [];
-                        products.forEach(product => {
-                            this.packages[product.id] = [];
-                            this.products.push(product);
-                        });
-
                         packages = json.data.packages || [];
-                        packages.forEach(package => {
-                            this.packages[package.product_id].push(package);
+                        colors = json.data.colors || [];
+                        this.package_statuses = json.data.package_statuses || [];
+                        this.show_prices = json.data.show_prices || [];
+
+                        products.forEach(product => {
+                            this.products.push(product);
+                            this.packages[product.id] = [];
                         });
 
-                        colors = json.data.colors || [];
+                        packages.forEach(package => {
+                            this.packages[package.product_id].push(JSON.parse(JSON.stringify(package)));
+                            this.packages_const[package.id] = JSON.parse(JSON.stringify(package));
+                        });
+
+                        console.log(this.packages_const);
+
                         colors.forEach(color => {
                             this.colors[color.id] = color;
                         });
-
-
 
                     } catch (e) {
                         console.log(e);
